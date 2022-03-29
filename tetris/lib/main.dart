57713b8a911713e0ed3a5a8b1d746c83bd734783
Yash Arora <yasharora9416@gmail.com>
2022-03-29 22:23:09 +0530

    Chore: Fixed Orientation

--- tetris/lib/main.dart
+++ tetris/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:tetris/game.dart';
 
+import 'package:flutter/services.dart';
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const MyApp());
 }
 
